2ce14ffa7efd6ca72baefbc873dc9c6e93e31305
limcecoder <45044526+platinumlimebot@users.noreply.github.com>
2018-11-20 17:52:42 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -4,15 +4,15 @@ module.exports.run = async (bot, message, args) => {
 
       const embed = new Discord.RichEmbed()
       
-      .setColor('#E74C3C')
-      .setTitle('All Commands')
-      .addField('Bot Owner', 'Lime#1224 Tag and Nick Owner Unchanged')
-      .addField('Support Server', 'http://bit.do/modsupport')
-      .addField('Invite Bot', 'http://bit.do/modinvite')
-      .addField('code', 'Code Created by Lime # 1224')
-      .addField('Thanks', 'Kick User Example + kick @mention <Reason>')
-      .addField('ban', 'Thanks to Heroku for a Free Host')
-      .setFooter(`Little Moderator `, `${message.author.displayAvatarURL}`)
+      .setColor("#E74C3C")
+      .setTitle("All Commands")
+      .addField("Bot Owner", "Lime#1224 Tag and Nick Owner Unchanged")
+      .addField("Support Server", "http://bit.do/modsupport")
+      .addField("Invite Bot", "http://bit.do/modinvite")
+      .addField("code", "Code Created by Lime # 1224")
+      .addField("Thanks", "Kick User Example + kick @mention <Reason>")
+      .addField("ban", 'Thanks to Heroku for a Free Host")
+      .setFooter(`Little Moderator ", `${message.author.displayAvatarURL}`)
       
 
       message.author.send({embed});
