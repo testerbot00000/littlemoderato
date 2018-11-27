50dea1c10c3b2f0dca8fa4535c9f7dbf1b4460d8
limcecoder <45044526+platinumlimebot@users.noreply.github.com>
2018-11-27 18:09:17 +0200

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let dUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+if (!dUser) return message.channel.send("Can't find user!")
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
+let dMessage = args.join(" ").slice(22);
+if(dMessage.length < 1) return message.reply('You must supply a message!')
+
+dUser.send(`${dUser} sent you: ${dMessage}`)
+
+message.author.send(`${message.author} You have sent your message to ${dUser}`)
+
+}
+module.exports.help = {
+    name: "dm"
+}
