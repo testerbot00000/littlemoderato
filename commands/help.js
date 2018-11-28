77da0128cc4656d5f5039c3aea62804f386b8bad
limcecoder <45044526+platinumlimebot@users.noreply.github.com>
2018-11-28 19:32:08 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-         
-      const embed = new Discord.RichEmbed()
-      
-          .setColor('#E74C3C')
-          .setTitle('All Commands')
-          .addField('say', 'Say from the name of the bot into the current channel')
-          .addField('dm', 'Send To PM On behalf of Bot')
-          .addField('clean', 'Clear Chat From 2 Post To 200')
-          .addField('kick', 'Kick User Example + kick @mention <Reason>')
-          .addField('ban', 'Ban User + ban @mention <Reason>')
-          .addField('Info','Get Inforamtion')
-          .setFooter(`Little Moderator Help`, `${message.author.displayAvatarURL}`)
-          
-      
-          message.author.send({embed})
-return message.reply(":postbox: | **Check PM** ");
+
+message.channel.send(":postbox: |**Vivsit http://bit.ly/littlemod-help For Help **");
       }
 module.exports.help = {
     name: "help"
