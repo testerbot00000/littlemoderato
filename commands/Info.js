d16ead25872eb882e1aee2a73f992294867778ce
limcecoder <45044526+platinumlimebot@users.noreply.github.com>
2018-12-02 12:07:25 +0200

    Add files via upload

---
+++ commands/Info.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    const m = await message.channel.send(`Number of Servers That Have a Bot ${bot.guilds.size}`);
+    message.channel.send(`Number of Users Who Use Bot ${bot.users.size}`)
+      }
+module.exports.help = {
+    name: "info"
+}
