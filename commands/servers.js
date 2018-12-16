b72c04385bf6f6cd46053060d4f3b6be68e6617e
limcecoder <45044526+platinumlimebot@users.noreply.github.com>
2018-12-16 12:45:04 +0200

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-bot.guilds.map(m => ${m.name} [${m.id}]).join('\n\n');
+bot.guilds.map(m => ${m.name} [${m.id}]).join('\n\n')
+message.channel.send(g);
 
       }
 module.exports.help = {
