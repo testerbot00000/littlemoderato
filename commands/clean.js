c65dc331b8b0f465f2fed5326f0795338eeb16f2
testerbot00000 <46234391+testerbot00000@users.noreply.github.com>
2018-12-30 15:03:49 -0500

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const deleteCount = parseInt(args[0], 10);
   
-    if(!deleteCount || deleteCount < 2 || deleteCount > 1000)
-      return message.reply("Please enter a number from 2 to 1000 for the number of messages to delete.");
+    if(!deleteCount || deleteCount < 2 || deleteCount > 200)
+      return message.reply("Please enter a number from 2 to 200 for the number of messages to delete.");
     
     const fetched = await message.channel.fetchMessages({count: deleteCount});
     message.channel.bulkDelete(fetched)
