3a596c16845ccb0a5406486af8a932ab021ff4dc
testerbot00000 <46234391+testerbot00000@users.noreply.github.com>
2018-12-30 09:29:19 -0500

    Update Info.js

--- commands/Info.js
+++ commands/Info.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    const m = await message.channel.send(`Number of Servers That Have a Bot ${bot.guilds.size}`);
-    message.channel.send(`Number of Users Who Use Bot ${bot.users.size}`)
+    const m = await message.channel.send(`Number of Servers That Have a Bot: ${bot.guilds.size}`);
+    message.channel.send(`Number of Users Who Use Bot: ${bot.users.size}`)
       }
 module.exports.help = {
     name: "info"
