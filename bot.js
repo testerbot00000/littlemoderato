a6449bc57ecd532f3d5c4a96824e50f9ff771bdf
testerbot00000 <46234391+testerbot00000@users.noreply.github.com>
2018-12-30 09:02:58 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,66 @@
+﻿const Discord = require('discord.js');
+const bot = new Discord.Client();
+const fs = require("fs");
+const ms = require("ms");
+const ytdl = require('ytdl-core');
+require('http').createServer().listen(3000)
+bot.commands = new Discord.Collection();
+fs.readdir("./commands", (err, files) => {
+    if(err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("Не могу найти команды!");
+        return;
+    }
+
+    jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} Команда Загружена`);
+    bot.commands.set(props.help.name, props);
+    });
+
+});
+var answers = [
+    `Little Moderator`,
+    `mod!help`,
+    `bit.ly/litllemod-invite`,
+    `bit.ly/mod-support`
+  ] 
+
+  bot.on('ready', () => {
+    let interval = setInterval (function () {
+        var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+        bot.user.setPresence({
+            game:{
+                name:`${randomAnswer}`,
+                type:"STREAMING",
+                url:"https://www.twitch.tv/monstercat"
+            }
+        });
+          }, 3 * 1000);
+      console.log('Я готов')
+    });
+bot.login(process.env.BOT_TOKEN);
+
+bot.on('message', async message => {
+    let prefix = 'mod!';
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot,message,args);
+});
+bot.on('messageUpdate', async (oldMessage, message) => {
+  let prefix = 'mod!';
+  if(message.author.bot) return;
+  if(!message.content.startsWith(prefix)) return;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+  });
+
